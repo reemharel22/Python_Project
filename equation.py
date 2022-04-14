32b60b5d63142f11e41fbeff694eeafef4d78ab9
Reem <reemharel22@gmail.com>
2022-04-14 14:11:25 +0300

    Added image

--- equation.py
+++ equation.py
@@ -1,1 +1,13 @@
+import numpy as np
+
+
+class Equation:
+    def __init__(self):
+        self.nx = 50
+        self.ny = 50
+        self.type = "Diffusion"
+
+
+    def Solve(self):
+        pass
 
--- gui.py
+++ gui.py
@@ -1,3 +1,3 @@
 import tkinter as tk
-
+import matplotlib
 
